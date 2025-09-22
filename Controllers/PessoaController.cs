b3e622a6726a73dcfff4aa0a64f11ec5da2d15a2
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-09-22 16:42:34 -0300

    Primeiro commit do projeto

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projetoPessoa.Models;
+
+namespace projetoPessoa.Controllers
+{
+    public class PessoaController : ControllerBase
+    {
+        List<Pessoa> listaPessoas = new List<Pessoa>
+        {
+            new Pessoa { Id = 1, Nome = "Ana Carolina", Idade = 30, Email = "anitadaflon@gmail.com" },
+            new Pessoa { Id = 2, Nome = "Igor BRandao", Idade = 25, Email = "devigorbrandao@gmail.com" }
+        };
+
+        [HttpGet]
+        [Route("api/pessoas")]
+        public IActionResult GetPessoas()
+        {
+            return Ok(listaPessoas);
+        }
+
+        [HttpGet]
+        [Route("api/pessoas/{id}")]
+        public IActionResult GetPessoaById(int id)
+        {
+            var pessoa = listaPessoas.FirstOrDefault(p => p.Id == id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+            return Ok(pessoa);
+        }
+
+        [HttpPost]
+        [Route("api/pessoas")]
+        public IActionResult CreatePessoa([FromBody] Pessoa novaPessoa)
+        {
+            if (novaPessoa == null)
+            {
+                return BadRequest();
+            }
+            else if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); 
+            }
+
+            novaPessoa.Id = listaPessoas.Max(p => p.Id) + 1;
+            listaPessoas.Add(novaPessoa);
+            return CreatedAtAction(nameof(GetPessoaById), new { id = novaPessoa.Id }, novaPessoa);
+        }
+
+        [HttpPut]
+        [Route("api/pessoas/{id}")]
+        public IActionResult UpdatePessoa(int id, [FromBody] Pessoa pessoaAtualizada)
+        {
+            var pessoa = listaPessoas.FirstOrDefault(p => p.Id == id);
+            if (pessoa == null || pessoaAtualizada == null)
+            {
+                return NotFound();
+            }
+            pessoa.Nome = pessoaAtualizada.Nome;
+            pessoa.Idade = pessoaAtualizada.Idade;
+            pessoa.Email = pessoaAtualizada.Email;
+            return NoContent();
+        }
+
+
+        [HttpDelete]
+        [Route("api/pessoas/{id}")]
+        public IActionResult DeletePessoa(int id)
+        {
+            var pessoa = listaPessoas.FirstOrDefault(p => p.Id == id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+            listaPessoas.Remove(pessoa);
+            return NoContent();
+        }
+
+    }
+}
+    
+    
+    
+    
---
+++ Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projetoPessoa.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+         
+
+    }
+}

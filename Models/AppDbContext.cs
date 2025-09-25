d78b3cdf2df9dee7d1106f8a906805cde9fbd505
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-09-25 18:08:34 -0300

    Criação da classe AppDbContext

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projetoPessoa.Models;
+
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }

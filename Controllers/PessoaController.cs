54bfb71633d9ebf78e6d9f8d1be90f28907c828e
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-09-25 17:58:27 -0300

    Alteração dos metodos na Api

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -1,28 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using projetoPessoa.Models;
+using System.Threading.Tasks;
 
 namespace projetoPessoa.Controllers
 {
     public class PessoaController : ControllerBase
     {
-        public static List<Pessoa> listaPessoas = new List<Pessoa>
+        private readonly AppDbContext _context;
+
+        public PessoaController(AppDbContext context)
         {
-            new Pessoa { Id = 1, Nome = "Ana Carolina", Idade = 30, Email = "anitadaflon@gmail.com" },
-            new Pessoa { Id = 2, Nome = "Igor BRandao", Idade = 25, Email = "devigorbrandao@gmail.com" }
-        };
+            _context = context;
+        }
+
 
         [HttpGet]
         [Route("api/pessoas")]
-        public IActionResult GetPessoas()
+        public async Task<IActionResult> GetPessoas()
         {
-            return Ok(listaPessoas);
+            var pessoas = await _context.Pessoas.ToListAsync();
+            return Ok(pessoas);
         }
 
         [HttpGet]
         [Route("api/pessoas/{id}")]
-        public IActionResult GetPessoaById(int id)
+        public async Task<IActionResult> GetPessoaById(int id)
         {
-            var pessoa = listaPessoas.FirstOrDefault(p => p.Id == id);
+            var pessoa = await _context.Pessoas.FirstOrDefaultAsync(p => p.Id == id);
             if (pessoa == null)
             {
                 return NotFound();
@@ -32,49 +37,57 @@ namespace projetoPessoa.Controllers
 
         [HttpPost]
         [Route("api/pessoas")]
-        public IActionResult CreatePessoa([FromBody] Pessoa novaPessoa)
+        public async Task<IActionResult> CreatePessoa([FromBody] Pessoa novaPessoa)
         {
             if (novaPessoa == null)
             {
                 return BadRequest();
             }
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState); 
-            }
 
-            novaPessoa.Id = listaPessoas.Any() ? listaPessoas.Max(p => p.Id) + 1 : 1;
-            listaPessoas.Add(novaPessoa);
+      
+            await _context.Pessoas.AddAsync(novaPessoa);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetPessoaById), new { id = novaPessoa.Id }, novaPessoa);
         }
 
         [HttpPut]
         [Route("api/pessoas/{id}")]
-        public IActionResult UpdatePessoa(int id, [FromBody] Pessoa pessoaAtualizada)
+        public async Task<IActionResult> UpdatePessoa(int id, [FromBody] Pessoa pessoaAtualizada)
         {
-            var pessoa = listaPessoas.FirstOrDefault(p => p.Id == id);
-            if (pessoa == null || pessoaAtualizada == null)
+            if (pessoaAtualizada == null)
+            {
+                return BadRequest();
+            }
+
+            var pessoa = await _context.Pessoas.FirstOrDefaultAsync(p => p.Id == id);
+            if (pessoa == null)
             {
                 return NotFound();
             }
+
             pessoa.Nome = pessoaAtualizada.Nome;
             pessoa.Idade = pessoaAtualizada.Idade;
             pessoa.Email = pessoaAtualizada.Email;
+
+            _context.Pessoas.Update(pessoa);
+            await _context.SaveChangesAsync();
+
             return NoContent();
         }
 
 
         [HttpDelete]
         [Route("api/pessoas/{id}")]
-        public IActionResult DeletePessoa(int id)
+        public async Task<IActionResult> DeletePessoa(int id)
         {
-            var pessoa = listaPessoas.FirstOrDefault(p => p.Id == id);
+            var pessoa = await _context.Pessoas.FirstOrDefaultAsync(p => p.Id == id);
             if (pessoa == null)
             {
                 return NotFound();
             }
-            listaPessoas.Remove(pessoa);
-            return NoContent();
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync(); 
+            return NoContent(); ;
         }
 
     }
